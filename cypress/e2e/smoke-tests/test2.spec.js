6dc89daf5670518d4b551bf37f14eec74b398e55
Ruslan Vasiliev <it.vasiliev@gmail.com>
2023-12-08 23:50:51 +0200

    commit hw10

---
+++ cypress/e2e/smoke-tests/test2.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+const baseUrl = "https://jsonplaceholder.typicode.com/posts/";
+const userId = 1;
+
+describe("test2 ", () => {
+  it("GET request ", () => {
+    cy.request("GET", baseUrl).then((response) => {
+      expect(response.status).eq(200);
+      expect(response.body).lengthOf(100);
+    });
+  });
+
+
+});
