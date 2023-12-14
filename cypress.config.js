4c0bb2791e693bb29dc6c9f5bef7edf4ac1e672a
Ruslan Vasiliev <it.vasiliev@gmail.com>
2023-12-14 12:58:33 +0200

    commit

---
+++ cypress.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      on("task", {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+      });
+    },
+    baseUrl: "http://localhost:4200",
+    specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
+  },
+  screenshotOnRunFailure: true, // default true
+  video: true, // default false
+  viewportHeight: 2000, // default 660
+  viewportWidth: 2000, // default 1200
+  // retries: 1,
+  defaultCommandTimeout: 5000, // default 4000
+  requestTimeout: 10000, // default 5000
+  responseTimeout: 30000, // default 30000
+
+  // watchForFileChanges: false,
+  env: {
+    POST: "4200",
+    WORK_ENV: "dev",
+    password: "qwerty",
+  },
+});
