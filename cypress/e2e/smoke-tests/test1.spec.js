84ef9d6c43ccf29146e7b59b7d706833a38fbff6
Ruslan Vasiliev <it.vasiliev@gmail.com>
2023-12-22 21:23:12 +0200

    CI__yml

---
+++ cypress/e2e/smoke-tests/test1.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+const baseUrl = "https://jsonplaceholder.typicode.com/posts/";
+const userId = 1;
+
+
+describe("test1", () => {
+  it("GET request ", () => {
+    cy.request("GET", baseUrl).then((response) => {
+      expect(response.status).eq(200);
+      expect(response.body).lengthOf(100);
+    });
+  });
+
+  it("POST request ", () => {
+    cy.request("POST", baseUrl, {
+      fixture: "user",
+    }).then((response) => {
+      expect(response.status).eq(201);
+      expect(response.statusText).eq("Created");
+    });
+  });
+
+
+});
