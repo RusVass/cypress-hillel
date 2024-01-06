2777bd96a9b1ca36b6b69c9a0c3f4803341cdd71
Ruslan Vasiliev <it.vasiliev@gmail.com>
2024-01-06 09:42:59 +0200

    commit lesson 16

--- cypress/e2e/lesson_16/hillel.auto.spec.js
+++ cypress/e2e/lesson_16/hillel.auto.spec.js
@@ -1,5 +1,4 @@
 /// <reference types="cypress" />
-
 describe("Form Layouts", () => {
   it("test qauto", () => {
     cy.visit("https://guest:welcome2qauto@qauto.forstudy.space/");
@@ -7,7 +6,35 @@ describe("Form Layouts", () => {
       email: Cypress.env("user"),
       password: Cypress.env("password"),
       remember: false,
+    })
+    cy.visit("https://guest:welcome2qauto@qauto.forstudy.space/panel/profile")
     });
-    cy.visit("https://guest:welcome2qauto@qauto.forstudy.space/panel/profile");
-  });
-});
+  })
+
+
+
+
+
+
+
+// describe("Form Layouts", () => {
+//   it("test qauto", () => {
+//     cy.visit("https://guest:welcome2qauto@qauto.forstudy.space/");
+//     cy.request("POST", "https://qauto.forstudy.space/api/auth/signin", {
+//       email: Cypress.env("user"),
+//       password: Cypress.env("password"),
+//       remember: false,
+//     }).then(res =>{
+//       const token = res.body.token
+//       console.log(token)
+//
+//
+//     cy.visit("https://guest:welcome2qauto@qauto.forstudy.space/panel/profile",{
+//       onBeforeLoad(win) {
+//         win.localStorage.setItem("auth-token", token)
+//       }
+//
+//     })
+//     });
+//   });
+// });
