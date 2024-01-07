e0a8f6f69a64e412a18b387c716393535d4c172a
Ruslan Vasiliev <it.vasiliev@gmail.com>
2024-01-07 15:28:39 +0200

    commit lesson-16

--- cypress/e2e/lesson_16/hillel.auto.spec.js
+++ cypress/e2e/lesson_16/hillel.auto.spec.js
@@ -1,19 +1,4 @@
 /// <reference types="cypress" />
-describe("Form Layouts", () => {
-  it("test qauto", () => {
-    cy.visit("https://guest:welcome2qauto@qauto.forstudy.space/");
-    cy.request("POST", "https://qauto.forstudy.space/api/auth/signin", {
-      email: Cypress.env("user"),
-      password: Cypress.env("password"),
-      remember: false,
-    })
-    cy.visit("https://guest:welcome2qauto@qauto.forstudy.space/panel/profile")
-    });
-  })
-
-
-
-
 
 
 
@@ -24,17 +9,33 @@ describe("Form Layouts", () => {
 //       email: Cypress.env("user"),
 //       password: Cypress.env("password"),
 //       remember: false,
-//     }).then(res =>{
-//       const token = res.body.token
-//       console.log(token)
-//
-//
-//     cy.visit("https://guest:welcome2qauto@qauto.forstudy.space/panel/profile",{
-//       onBeforeLoad(win) {
-//         win.localStorage.setItem("auth-token", token)
-//       }
-//
 //     })
+//     cy.visit("https://guest:welcome2qauto@qauto.forstudy.space/panel/profile")
 //     });
-//   });
-// });
+//   })
+
+
+
+
+
+
+
+describe("Form Layouts", () => {
+  it("test qauto", () => {
+    cy.visit("https://guest:welcome2qauto@qauto.forstudy.space/");
+    cy.request("POST", "https://qauto.forstudy.space/api/auth/signin", {
+      email: Cypress.env("user"),
+      password: Cypress.env("password"),
+      remember: false,
+    }).then(res =>{
+      const token = res.body.token
+      console.log(token)
+
+    cy.visit("https://guest:welcome2qauto@qauto.forstudy.space/panel/profile",{
+      onBeforeLoad(win) {
+        win.localStorage.setItem("auth-token", token)
+      }
+    })
+    });
+  });
+});
